c119704b83c7b7653f24d21cec4b5d205dd2ef76
Scott Morgan <scott@adligo.com>
2023-04-20 09:25:30 -0500

    Backing up work --help is working yea.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # slink.ts.adligo.org
 This is a simple Type Script Application that will create and store symbolic link data into your package.json file under the slinks section. This can help you improve the speed of changing upstream Javascript or Typescript in a multiple project dependency system.
 
+
+
+## Uninstall the package;
+```
+npm uninstall -g @ts.adligo.org/slink
+```
+
+## Compile / Run the TypeScript Compile (creates a bin and dist folder)
+```
+ npm run tsc
+```
+
+## Development Install it from the local code
+```
+npm install -g .
+```
--- src/slink.ts
+++ src/slink.ts
@@ -20,30 +20,147 @@ import {I_Out} from '@ts.adligo.org/io';
 
 const foo : I_Out = (foo) => console.log(foo);
 
-function removeChars(s: string, chars: string[]): string {
-  var r = '';
-  for (var i=0; i < s.length; i ++) {
-    let c = s.charAt(i);
-    var t = true;
-    for (var j=0; j < chars.length; j++) {
-      let cj = chars[j];
-      if (c == cj) {
-        t = false;
-        break;
-      }
-    }
-    if (t) {
-      //console.log('adding ' + c);
-      r = r.concat(c);
+/**
+ * This is a set of attributes that can be used on the Command Line as
+ * an Argument.
+ */
+export interface I_CliCtxFlag {
+  /**
+   * The full command that will be expected if the double dash (i.e. --help )is used
+   */
+  cmd: string;
+  /**
+   * This is the description of what the command should do
+   */
+  description?: string;
+  /**
+   * This indicates if the flag accepts arguments or is simply a flag,
+   * it defaults to true.
+   */
+  flag?: boolean;
+  /**
+   * This is the single letter that can be concatinated together (i.e. f and r in rm -fr)
+   */
+  letter?: string;
+}
+
+export class CliCtxFlag {
+  private cmd: string;
+  private letter?: string;
+  private description?: string;
+  private flag?: boolean;
+
+  constructor(flag: I_CliCtxFlag) {
+    this.cmd = flag.cmd;
+    this.letter = flag.letter;
+    this.description = flag.description;
+    if (flag.flag == undefined) {
+      this.flag = true;
     } else {
-      //console.log('ignoring ' + c);
+      this.flag = flag.flag;
     }
   }
-  return r;
+  getCmd(): string { return this.cmd; }
+  getDescription(): string { return this.description; }
+  getFlag(): boolean { return this.flag; }
+  getLetter(): string { return this.letter; }
+  isFlag(): boolean { return this.flag; }
+}
+
+export class CliCtxArg {
+  private flag: CliCtxFlag; 
+  private arg?: string;
+
+  constructor(flag: CliCtxFlag, arg? : string) {
+    this.flag = flag;
+    this.arg = arg;
+  }
+  getArg(): string { return this.arg}
+  getFlag(): CliCtxFlag { return this.flag}
+}
+
+export class Strings {
+  static removeChars(s: string, chars: Set<string>): string {
+    var r = '';
+    for (var i=0; i < s.length; i ++) {
+      let c = s.charAt(i);
+      var t = true;
+      if (chars.has(c)) {
+        //console.log('ignoring ' + c);
+      } else {
+        //console.log('adding ' + c);
+        r = r.concat(c);
+      }
+    }
+    return r;
+  }
+}
+
+export class Paths {
+  /**
+   * 
+   * @param path a fully qualified path
+   * @returns 
+   */
+  static toUnix(path: string): string {
+    var r = '';
+    if (path.length > 3) {
+      if (path.charAt(1) == ':') {
+        r = '/c/';
+        for (var i=3; i< path.length; i++) {
+          let c = path[i];
+          if (c == '\\') {
+            r = r.concat('/');
+          } else {
+            r = r.concat(c);
+          }
+        }
+      } else {
+        //assume unix path already!
+        return path;
+      }
+    } else {
+      throw Error("Unable to parse paths of length 3 or smaller!");
+    }
+    return r;
+  }
+
+  static toUnixPath(parts: string[]): string {
+    let b = '/';
+    for (var i=0; i < parts.length; i++) {
+      b = b.concat(parts[i]).concat('/');
+    }
+    return b;
+  }
+  /**
+   * 
+   * @param unixPath a fully qualified path
+   */
+  static toParts(unixPath: string): string[] {
+    let r: string[] = new Array();
+    let b = '';
+    var j = 0;
+    if (unixPath.charAt(0) != '/') {
+      throw Error('The following unixPath is not a fully qualified path!;\n' + unixPath);
+    }
+    for (var i=1; i< unixPath.length; i++) {
+      let c = unixPath[i];
+      if (c == '/') {
+        if (b.length != 0) {
+          r[j] = b;
+          b = '';
+          j++;
+        }
+      } else {
+        b = b.concat(c);
+      }
+    }
+    return r;
+  }
 }
 
 const UnixCmdError1 = 'Unable to run the UNIX ';
-const UnixCmdError2 = ' command, if your running on Windows try using GitBash to run this program!\n';
+const UnixCmdError2 = ' command!\nIf your running on Windows try using GitBash to run this program!\n';
 class Pwd {
   private path: string;
 
@@ -51,29 +168,124 @@ class Pwd {
     try {
       let pwd = spawnSync('pwd',[]);
       var pi = "" + pwd.output;
-      console.log('pwd output is ' + pwd.output);
+      //console.log('pwd output is ' + pwd.output);
       if ('null' == pi) {
         throw Error('The output from pwd is ' + pi);
       }
-      this.path = removeChars(pi, [',','\n']);
+      this.path = Strings.removeChars(pi,new Set<string>().add(',').add('\n'));
     } catch (e) {
       throw Error(UnixCmdError1 + 'pwd' + UnixCmdError2 + e);
     }
   }
   public toPath() { return this.path; }
 }
-class CliArgParser {
+const HELP: I_CliCtxFlag = {cmd: "help", description: "Displays the Help Menu, prints this output."}
+const VERSION: I_CliCtxFlag = {cmd: "version", description: "Displays the version.", flag: true, letter: "v"}
 
-  constructor() {
-    console.log('running in ' + new Pwd().toPath());
-    console.log('There are ' + process.argv.length + ' command line arguments');
-    for (var i=0; i< process.argv.length; i++) {
-      console.log('cli arg ' + i + ' is ' + process.argv[i]);
+class CliCtx {
+  private done: boolean = false;
+  private path: string;
+  private home: string;
+  private map: Map<string, CliCtxArg> = new Map();
+
+  /**
+   * 
+   * @param flags 
+   * @param args 
+   * @param belowRoot this is an integer which identifies the number of 
+   *   path parts below the projects (npm module) root root the actual
+   *   CLI script is.  This defaults to 1 for scripts in the src dir.
+   * @param out 
+   */
+  constructor(flags: I_CliCtxFlag[], args?: string [], belowRoot?: number, out?: I_Out) {
+    this.path = new Pwd().toPath();
+    if (args == undefined) {
+      args = process.argv;
+    }
+    if (belowRoot == undefined) {
+      belowRoot = 1;
+    }
+    if (out == undefined) {
+      out = (message) => console.log(message);
+    }
+    let allFlags: CliCtxFlag[] = new Array(flags.length);
+    let map2Cmds: Map<string, CliCtxFlag> = new Map();
+    let map2Letters: Map<string, CliCtxFlag> = new Map();
+    for (var i=0; i< flags.length; i++) {
+      let f = new CliCtxFlag(flags[i]);
+      allFlags[i] = f;
+      if (map2Cmds.has(f.getCmd())) {
+        throw Error("The following command has been duplicated? " + f.getCmd())
+      }
+      map2Cmds.set(f.getCmd(), f);
+      if (f.getLetter() != undefined) {
+        if (map2Letters.has(f.getLetter())) {
+          throw Error("The following command has a duplicated letter? " + f.getCmd())
+        }
+        map2Letters.set(f.getLetter(), f);
+      }
+    }
+    this.home = Paths.toUnix(args[1]);
+    let homeParts: string[] = Paths.toParts(this.home);
+    this.home = Paths.toUnixPath(homeParts.slice(0, homeParts.length - belowRoot))
+    for (var i=2; i< args.length; i++) {
+      let a = args[i];
+      out('processing cli arg ' + a);
+      if (a.length < 2) {
+        let a = i -1;
+        throw Error('Unable to parse command line arguments, issue at argument; ' + a);
+      } else {
+        let dd = a.substring(0,2);
+        if (dd == '--') {
+          let cmd = a.substring(2, a.length);
+          out('cmd is ' + cmd);
+          let flag: CliCtxFlag = map2Cmds.get(cmd);
+          if (flag.isFlag()) {
+            this.map.set(cmd, new CliCtxArg(flag));            
+          } else if (args.length < i + 1) {
+            let arg = args[i + 1];
+            i++;
+            this.map.set(cmd, new CliCtxArg(flag, arg)); 
+          } else {
+            throw Error('The following command line argument expects an additional argument; ' + cmd);
+          }
+        } else if (a.charAt(0) == '-') {
+          //process letters
+          for (var j=1; j < a.length; j++) {
+            let l = a.charAt(j);
+            out('processing letter ' + l);
+            let flag: CliCtxFlag = map2Letters.get(l);
+            out('got command ' + flag.getCmd() + ' for letter ' + l);
+            this.map.set(flag.getCmd(), new CliCtxArg(flag));  
+          }
+        } else {
+          throw Error('Unable to process command line argument ; ' + a);
+        }
+      } 
+    }
+    if (this.map.get(HELP.cmd)) {
+      //print the help menu;
+      out('This program understands the following commands;\n');
+      for (var i = 0; i < flags.length; i++) {
+        let flag: I_CliCtxFlag = flags[i];
+        var m = '\t--' + flag.cmd;
+        if (flag.letter != undefined) {
+          m = m + ' / ' + flag.letter;
+        }
+        out(m);
+        if (flag.description != undefined) {
+          out('\t\t' + flag.description);
+        }
+      }
+    } else if (this.map.get(VERSION.cmd)) {
+
     }
   }
+  getHome(): string { return this.home; }
 }
 
 console.log('hello slink2 ');
-new CliArgParser();
-console.log('CliArgParser created');
+let flags: I_CliCtxFlag[] = [HELP, VERSION];
+let ctx = new CliCtx(flags);
+console.log('CliArgParser created with home\n\t' + ctx.getHome());
 
